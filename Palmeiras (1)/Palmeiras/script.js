a019b04f0cb7f8be5ee298146961dd16788188b9
Júlia Nova <144247648+julianova23@users.noreply.github.com>
2023-09-15 08:09:35 -0300

    Versão 1.0

---
+++ Palmeiras (1)/Palmeiras/script.js
@@ -0,0 +1,70 @@
+function Veiga() {
+    const myJSON_jogadores = ["Raphael Cavalcante Veiga", "28 anos", "19 de junho de 1995", "São Paulo, Sp", "Jogador, meio do Palmeiras", "Camisa 23"];
+
+    const array_jogadores = JSON.parse(JSON.stringify(myJSON_jogadores));
+
+document.getElementById("sobre").innerHTML =
+    "<p>Nome: " + array_jogadores[0] + "</p>" +
+    "<p>Idade: " + array_jogadores[1] + "</p>" + 
+    "<p>Nascimento: " + array_jogadores[2] + "</p>"+
+    "<p>Local de Nascimento: " + array_jogadores[3] + "</p>"+
+    "<p>Profissão: " +  array_jogadores[4] + "</p>" + "<p>Camisa: " +  array_jogadores[5] + "</p>" +
+    "<p><img src='rapha.jpg' alt='' " + array_jogadores[6] + "</p>"
+}
+
+function Murilo() {
+    const myJSON_jogadores = ["Murilo Cerqueira", "26 anos", "27 de março de 1997","São Gonçalo dos Campos, Bahia", "Jogador, zagueiro do Palmeiras", "Camisa 26"];
+
+    const array_jogadores = JSON.parse(JSON.stringify(myJSON_jogadores));
+
+document.getElementById("sobre").innerHTML =
+    "<p>Nome: " + array_jogadores[0] + "</p>" +
+    "<p>Idade: " + array_jogadores[1] + "</p>" + 
+    "<p>Nascimento: " + array_jogadores[2] + "</p>"+
+    "<p>Local de Nascimento: " + array_jogadores[3] + "</p>"+
+    "<p>Profissão: " +  array_jogadores[4] + "</p>" + "<p>Camisa: " +  array_jogadores[5] + "</p>" +
+    "<p><img src='mumu.jpg' alt='' " + array_jogadores[6] + "</p>"
+}
+
+
+function Dudu() {
+    const myJSON_jogadores = ["Eduardo Pereira Rodrigues", "31 anos", "7 de janeiro de 1992", "Goiânia, Goiás", "Jogador, atacante do Palmeiras", "Camisa 7"];
+
+    const array_jogadores = JSON.parse(JSON.stringify(myJSON_jogadores));
+
+document.getElementById("sobre").innerHTML =
+    "<p>Nome: " + array_jogadores[0] + "</p>" +
+    "<p>Idade: " + array_jogadores[1] + "</p>" + 
+    "<p>Nascimento: " + array_jogadores[2] + "</p>"+
+    "<p>Local de Nascimento: " + array_jogadores[3] + "</p>"+
+    "<p>Profissão: " +  array_jogadores[4] + "</p>"+ "<p>Camisa: " +  array_jogadores[5] + "</p>"+
+    "<p><img src='duds.jpg' alt='' " + array_jogadores[6] + "</p>"
+}
+
+function ZéRafael() {
+    const myJSON_jogadores = ["José Rafael Vivian", "30 anos", "16 de junho de 1993", " Ponta Grossa, Paraná", "Jogador, volante do Palmeiras", "Camisa 8"];
+
+    const array_jogadores = JSON.parse(JSON.stringify(myJSON_jogadores));
+
+document.getElementById("sobre").innerHTML =
+    "<p>Nome: " + array_jogadores[0] + "</p>" +
+    "<p>Idade: " + array_jogadores[1] + "</p>" + 
+    "<p>Nascimento: " + array_jogadores[2] + "</p>"+
+    "<p>Local de Nascimento: " + array_jogadores[3] + "</p>"+
+    "<p>Profissão: " +  array_jogadores[4] + "</p>"+ "<p>Camisa: " +  array_jogadores[5] + "</p>"+
+    "<p><img src='zeze.jpg' alt='' " + array_jogadores[6] + "</p>"
+}
+
+function Abel() {
+    const myJSON_jogadores = ["Abel Fernando Moreira Ferreira", "44 anos", "22 de dezembro de 1978", "Penafiel, Portugal", "Comandante do Palmeiras, sendo um dos maiores e mais marcantes técnicos da história do clube e do Brasil"];
+
+    const array_jogadores = JSON.parse(JSON.stringify(myJSON_jogadores));
+
+document.getElementById("sobre").innerHTML =
+    "<p>Nome: " + array_jogadores[0] + "</p>" +
+    "<p>Idade: " + array_jogadores[1] + "</p>" + 
+    "<p>Nascimento: " + array_jogadores[2] + "</p>"+
+    "<p>Local de Nascimento: " + array_jogadores[3] + "</p>"+
+    "<p>Profissão: " +  array_jogadores[4] + "</p>"+
+    "<p><img src='abelzinho.jpg' alt='' " + array_jogadores[5] + "</p>"
+}
